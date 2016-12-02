a3c9128e132f8bdc7405177b8f9aa0fc940c2dcd
Sercammus <sercammus@gmail.com>
2016-12-02 12:32:35 +0100

    Revert "Created 'Hello World' implementation"
    This reverts commit c1f6348977df268f8caf4bcfd446f49b550f4ceb.

--- GitTestRepJavaProject/src/main/Program.java
+++ GitTestRepJavaProject/src/main/Program.java
@@ -3,11 +3,8 @@ package main;
 public class Program {
 
 	public static void main(String[] args) {
-		printMsg("Hello world!");
-	}
-	
-	public static void printMsg(String msg) {
-		System.out.println(msg);
+		// TODO Auto-generated method stub
+
 	}
 
 }
