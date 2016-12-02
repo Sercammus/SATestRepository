c665e83147c036651c8741cc6ffe356f05ee4254
Sercammus <sercammus@gmail.com>
2016-12-02 12:39:20 +0100

    Printing messages multiple times

--- GitTestRepJavaProject/src/main/Program.java
+++ GitTestRepJavaProject/src/main/Program.java
@@ -3,11 +3,13 @@ package main;
 public class Program {
 
 	public static void main(String[] args) {
-		printMsg("Hello world!");
+		printMsg("Hello world!", 3);
 	}
 	
-	public static void printMsg(String msg) {
-		System.out.println(msg);
+	public static void printMsg(String msg, int count) {
+		for (int index = 1; index <= count; index++) {
+			System.out.println(msg);
+		}
 	}
 
 }
